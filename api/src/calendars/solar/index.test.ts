236de81f8070ad67f0c90b63ebf71b818ba9b959
Pedro Martin <pataruco@gmail.com>
2021-05-03 13:15:21 +0100

    Test getSolar element

--- api/src/calendars/solar/index.test.ts
+++ api/src/calendars/solar/index.test.ts
@@ -1,9 +1,20 @@
 import getSolarElement from './index';
 
-//TODO: https://stackoverflow.com/questions/67324257/syntaxerror-cannot-use-import-statement-outside-a-module-how-to-run-test-unit
-
 describe(getSolarElement, () => {
-  it('return an element when a date is given', () => {
-    expect(true).toBe(true);
+  const juneSolstice = new Date('2021-06-21');
+  const decemberSolstice = new Date('2021-12-21');
+  const marchEquinox = new Date('2021-03-21');
+  const septemberEquinox = new Date('2021-09-21');
+  const firstOfMay = new Date('2021-05-01');
+
+  test.each`
+    date                | expected
+    ${juneSolstice}     | ${'FIRE'}
+    ${decemberSolstice} | ${'WATER'}
+    ${marchEquinox}     | ${'WOOD'}
+    ${septemberEquinox} | ${'METAL'}
+    ${firstOfMay}       | ${'EARTH'}
+  `('returns $expected when date is $date', ({ date, expected }) => {
+    expect(getSolarElement(date)).toBe(expected);
   });
 });
