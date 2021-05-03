4770d9644fcf49648e1fa28c53c6ac5574aa5af2
Pedro Martin <pataruco@gmail.com>
2021-05-03 13:22:10 +0100

    Scaffold getLunarElement

--- api/@types/element.d.ts
+++ api/@types/element.d.ts
@@ -2,4 +2,5 @@ export type Element = 'WOOD' | 'FIRE' | 'EARTH' | 'METAL' | 'WATER';
 
 export interface GetElements {
   solar: Element;
+  lunar: Element;
 }
---
+++ api/src/calendars/lunar/index.ts
@@ -0,0 +1,7 @@
+import { GetElements } from '../../../@types';
+
+const getLunarElement = (date: Date): GetElements['lunar'] => {
+  return 'EARTH';
+};
+
+export default getLunarElement;
