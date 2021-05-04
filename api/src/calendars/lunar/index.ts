416ffe536b3094124c1cbedadd93367db2c65fe1
Pedro Martin <pataruco@gmail.com>
2021-05-04 11:45:52 +0100

    Non metal elements working

--- api/src/calendars/lunar/index.ts
+++ api/src/calendars/lunar/index.ts
@@ -1,53 +1,69 @@
-import {
-  MoonQuarter,
-  NextMoonQuarter,
-  SearchMoonQuarter,
-  MoonPhase,
-} from 'astronomy-engine';
+import { MoonPhase } from 'astronomy-engine';
 
 import '../../lib/date';
 import { GetElements } from '../../../@types';
 
 const DAYS_RANGE = 2;
-// the difference in ecliptic longitude between the center of the Sun and the center of the Moon /
-// The lunar phases gradually change over a synodic month(about 29.53 days) as the Moon's orbital positions around Earth and Earth around the Sun shift.
+// the difference in ecliptic longitude between the center of the Sun and the center of the Moon
+// divided by
+// a synodic month(about 29.53 days) as the Moon's orbital positions around Earth and Earth around the Sun shift.
 
 const DEGREES_PER_DAY = Number((359 / 29.53).toPrecision(6)); // 12.1571
+// const DEGREES_PER_DAY = 12;
 
-interface GetMoonPhaseRange {
-  currentMoonQuarter: MoonQuarter;
-  nextMoonQuarter: MoonQuarter;
+const isInWaterRange = (moonPhaseDegrees: number): boolean => {
+  console.log({
+    moonPhaseDegrees,
+    start: 360 - DAYS_RANGE * DEGREES_PER_DAY,
+    end: 0 + DAYS_RANGE * DEGREES_PER_DAY,
+  });
+
+  return (
+    moonPhaseDegrees >= 360 - (DAYS_RANGE + 1) * DEGREES_PER_DAY &&
+    moonPhaseDegrees <= 0 + DAYS_RANGE * DEGREES_PER_DAY
+  );
+};
+
+interface IsInElementRange {
+  moonPhaseDegrees: number;
+  rangeStart: 0 | 90 | 180 | 270 | 360;
 }
 
-// const isInMoonRange = (
-//   currentMoonPhaseDegrees: number,
-//   moonPhaseDegrees: number,
-// ): boolean => {
-//   return (
-//   );
-// };
+const isInElementRange = ({
+  moonPhaseDegrees,
+  rangeStart,
+}: IsInElementRange): boolean =>
+  moonPhaseDegrees >= rangeStart - (DAYS_RANGE + 1) * DEGREES_PER_DAY &&
+  moonPhaseDegrees <= rangeStart + DAYS_RANGE * DEGREES_PER_DAY;
 
 const getLunarElement = (date: Date): GetElements['lunar'] => {
   date.setUTCHours(0, 0, 0, 0); // Set beginning of the day
 
   const moonPhaseDegrees = MoonPhase(date);
 
-  // TODO: calculate if current moon phase is between the range of days
+  /*
+0 = new moon
+90 = first quarter
+180 = full moon
+270 = third quarter
 
-  // switch (true) {
-  //   case value:
-  //     break;
+  */
 
-  //   default:
-  //     break;
-  // }
-
-  console.log({
-    moonPhaseDegrees: moonPhaseDegrees.toFixed(6),
-    DEGREES_PER_DAY,
-  });
-
-  return 'EARTH';
+  switch (true) {
+    case isInWaterRange(moonPhaseDegrees):
+      return 'WATER';
+    // 90 = first quarter
+    case isInElementRange({ moonPhaseDegrees, rangeStart: 90 }):
+      return 'WOOD';
+    // 180 = full moon
+    case isInElementRange({ moonPhaseDegrees, rangeStart: 180 }):
+      return 'FIRE';
+    // 270 = third quarter
+    case isInElementRange({ moonPhaseDegrees, rangeStart: 270 }):
+      return 'METAL';
+    default:
+      return 'EARTH';
+  }
 };
 
 export default getLunarElement;
