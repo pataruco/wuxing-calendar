10c29372f87d078f5423c14bcad7a5d8ccdbf7df
Pedro Martin <pataruco@gmail.com>
2021-05-05 15:07:21 +0100

    Create a page

---
+++ web/src/pages/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Page from '../components/page';
+
+const Home: React.FC = () => {
+  return (
+    <Page>
+      <h1>Home</h1>
+    </Page>
+  );
+};
+
+export default Home;
