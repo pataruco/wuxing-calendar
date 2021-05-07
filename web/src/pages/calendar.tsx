0b42c71ba3441f3d12e1530f7888b3cb1ee4d205
Pedro Martin <pataruco@gmail.com>
2021-05-07 11:32:57 +0100

    Able to get dates from calendar

--- web/src/pages/calendar.tsx
+++ web/src/pages/calendar.tsx
@@ -1,6 +1,50 @@
 import React from 'react';
+import FullCalendar, {
+  CustomContentGenerator,
+  EventContentArg,
+  EventSourceFunc,
+  EventSourceInput,
+} from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+const getEvents: EventSourceFunc = (
+  fetchInfo,
+  successCallback,
+  failureCallback,
+) => {
+  const { start, end } = fetchInfo;
+  console.log({ start, end });
+};
+
+const eventSources: EventSourceInput[] = [
+  // your event source
+  {
+    events: getEvents,
+    color: 'yellow', // an option!
+    textColor: 'black', // an option!
+  },
+  // any other sources...
+];
 
 const Calendar: React.FC = () => {
-  return <h1>Calendart</h1>;
+  //Identity<CustomContentGenerator<EventContentArg>>;
+  const handleEventContent: CustomContentGenerator<EventContentArg> = (
+    event,
+  ) => {
+    console.log({ event });
+  };
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      eventContent={handleEventContent}
+      // events={[
+      //   { title: 'event 1', date: '2021-05-07' },
+      //   { title: 'event 2', date: '2021-05-08' },
+      // ]}
+      eventSources={eventSources}
+    />
+  );
 };
 export default Calendar;
