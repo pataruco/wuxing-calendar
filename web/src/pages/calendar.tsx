42e2042161eeda0c8211719d98fc662fe7e4a614
Pedro Martin <pataruco@gmail.com>
2021-05-07 12:56:25 +0100

    Render solar calendar

--- web/src/pages/calendar.tsx
+++ web/src/pages/calendar.tsx
@@ -2,13 +2,51 @@ import React from 'react';
 import FullCalendar, {
   CustomContentGenerator,
   EventContentArg,
+  EventInput,
   EventSourceFunc,
   EventSourceInput,
 } from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
-import { getCalendarPhases } from '../redux/calendar/actions';
+// import { getCalendarPhases } from '../redux/calendar/actions';
 import { selectCalendar } from '../redux/calendar';
+import GetPhases from 'five-phases';
+
+interface GetCalendarPhases {
+  start: Date;
+  end: Date;
+}
+const getDates = ({ start, end }: GetCalendarPhases): Date[] => {
+  let dates: Date[] = [];
+  //to avoid modifying the original date
+  const theDate = new Date(start);
+  while (theDate < end) {
+    dates = [...dates, new Date(theDate)];
+    theDate.setDate(theDate.getDate() + 1);
+  }
+  return dates;
+};
+
+export const getCalendarPhases = ({
+  start,
+  end,
+}: GetCalendarPhases): EventInput[] => {
+  const dates = getDates({ start, end });
+  const eventPhases: EventInput[] = dates.map((date) => {
+    const { solar, lunar, hour } = GetPhases({
+      date,
+      exact: false,
+      hemisphere: 'NORTHERN',
+    });
+
+    return {
+      title: `Solar ${solar}`,
+      start: date.toISOString(),
+      classNames: ['solar', solar],
+    };
+  });
+  return eventPhases;
+};
 
 const Calendar: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -23,7 +61,8 @@ const Calendar: React.FC = () => {
     failureCallback,
   ) => {
     const { start, end } = fetchInfo;
-    dispatch(getCalendarPhases({ start, end }));
+    const events = getCalendarPhases({ start, end });
+    successCallback(events);
   };
 
   const eventSources: EventSourceInput[] = [
@@ -40,12 +79,13 @@ const Calendar: React.FC = () => {
     <FullCalendar
       plugins={[dayGridPlugin]}
       initialView="dayGridMonth"
-      events={[
-        { title: 'event 1', date: '2021-05-07' },
-        { title: 'event 2', date: '2021-05-08' },
-      ]}
+      // events={[
+      //   { title: 'event 1', date: '2021-05-07' },
+      //   { title: 'event 2', date: '2021-05-08' },
+      // ]}
       eventSources={eventSources}
     />
   );
 };
+
 export default Calendar;
