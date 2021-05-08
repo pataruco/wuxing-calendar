57c09abdda35c3aab2939823f8c49f8871fb13fd
Pedro Martin <pataruco@gmail.com>
2021-05-08 20:05:05 +0100

    Style footer

--- web/src/components/footer/index.tsx
+++ web/src/components/footer/index.tsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Footer: React.FC = () => {
-  return <footer>footer</footer>;
-};
+const StyledFooter = styled.footer`
+  background-color: var(--black);
+  padding: 1.25rem;
+  display: flex;
+  justify-content: space-between;
+
+  p {
+    color: var(--white);
+    text-align: center;
+    max-width: 100%;
+    span {
+      color: red;
+    }
+
+    a {
+      color: var(--white);
+      &:hover {
+        text-decoration: underline 2px solid white;
+      }
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    & {
+      flex-direction: column;
+      justify-content: flex-start;
+    }
+
+    p {
+      text-align: start;
+      &:first-of-type {
+        margin-top: 0;
+      }
+      &:last-of-type {
+        margin-bottom: 0;
+      }
+    }
+  }
+`;
+
+const Footer: React.FC = () => (
+  <StyledFooter>
+    <p>
+      Made with{' '}
+      <span role="img" aria-labelledby="heart">
+        ❤️
+      </span>{' '}
+      by{' '}
+      <a href="https://github.com/pataruco" target="_blank" rel="noreferrer">
+        @pataruco
+      </a>{' '}
+      {new Date().getFullYear()}
+    </p>
+  </StyledFooter>
+);
 
 export default Footer;
