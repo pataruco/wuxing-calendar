08f3a4a7f3a5fa3d66a699d5410b230b00f8d2e5
Pedro Martin <pataruco@gmail.com>
2021-05-08 17:52:47 +0100

    Set locale

--- web/src/pages/calendar/index.tsx
+++ web/src/pages/calendar/index.tsx
@@ -4,18 +4,25 @@ import React from 'react';
 
 import { eventSources } from './helpers';
 import { getUserLocales } from '../../lib/get-locale';
+import Page from '../../components/page';
 
 const Calendar: React.FC = () => {
-  const locales = getUserLocales();
-
-  console.log({ locales });
+  const [locale] = getUserLocales();
 
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin]}
-      initialView="dayGridMonth"
-      eventSources={eventSources}
-    />
+    <Page>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        eventSources={eventSources}
+        locale={locale}
+        firstDay={1}
+        aspectRatio={2.5}
+        dayHeaderFormat={{
+          weekday: 'long',
+        }}
+      />
+    </Page>
   );
 };
 
