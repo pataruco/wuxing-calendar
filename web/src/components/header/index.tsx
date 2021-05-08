84efee3056c18af9c5b4b37e1d27d2bc7d65b51c
Pedro Martin <pataruco@gmail.com>
2021-05-08 20:05:05 +0100

    Style footer

--- web/src/components/header/index.tsx
+++ web/src/components/header/index.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 import { useHistory, Link } from 'react-router-dom';
 
-const StyledHeader = styled.header``;
+const StyledHeader = styled.header`
+  padding: 1.25rem;
+  display: flex;
+  justify-content: flex-end;
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 const Header: React.FC = () => {
   const history = useHistory();
