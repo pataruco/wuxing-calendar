558254aa92dfe459fb4c2359c8c0b1239b4561a0
Pedro Martin <pataruco@gmail.com>
2021-05-08 18:04:15 +0100

    Set logic for header

--- web/src/components/header/index.tsx
+++ web/src/components/header/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useHistory, Link } from 'react-router-dom';
+
+const StyledHeader = styled.header``;
 
 const Header: React.FC = () => {
-  return <header>header</header>;
+  const history = useHistory();
+
+  const {
+    location: { pathname },
+  } = history;
+
+  const isHome = pathname === '/';
+
+  return (
+    <StyledHeader>
+      <nav>
+        <ul>
+          {isHome ? (
+            <li>
+              <Link to="/calendar">Calendar</Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/">Now</Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </StyledHeader>
+  );
 };
 
 export default Header;
