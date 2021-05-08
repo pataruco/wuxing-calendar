d85c3529678f1dc0ea2e7b838167826f5c8195f7
Pedro Martin <pataruco@gmail.com>
2021-05-08 22:57:51 +0100

    Style calendar

--- web/src/pages/calendar/index.tsx
+++ web/src/pages/calendar/index.tsx
@@ -5,12 +5,49 @@ import React from 'react';
 import { eventSources } from './helpers';
 import { getUserLocales } from '../../lib/get-locale';
 import Page from '../../components/page';
+import styled from 'styled-components';
+
+const StyledPage = styled(Page)`
+  --fc-border-color: black;
+  --fc-button-bg-color: black;
+  --fc-button-border-color: white;
+
+  .fc-daygrid-event {
+    padding: 0.5rem;
+    font-size: 1.15rem;
+
+    &.wood {
+      background-color: var(--wood);
+      border-color: var(--wood);
+    }
+    &.fire {
+      background-color: var(--fire);
+      border-color: var(--fire);
+    }
+    &.earth {
+      background-color: var(--earth);
+      border-color: var(--earth);
+    }
+    &.metal {
+      background-color: var(--metal);
+      border-color: var(--metal);
+    }
+    &.water {
+      background-color: var(--water);
+      border-color: var(--water);
+    }
+  }
+
+  .fc-day-today {
+    box-shadow: 1px 1px 1px 1px black, -1px -1px 1px 1px black;
+  }
+`;
 
 const Calendar: React.FC = () => {
   const [locale] = getUserLocales();
 
   return (
-    <Page>
+    <StyledPage>
       <FullCalendar
         plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
@@ -22,7 +59,7 @@ const Calendar: React.FC = () => {
           weekday: 'long',
         }}
       />
-    </Page>
+    </StyledPage>
   );
 };
 
