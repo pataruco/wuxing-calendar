89afec9c2dadf4c1ac69bdb1af4441a30bb6cb10
Pedro Martin <pataruco@gmail.com>
2021-05-09 19:48:00 +0100

    Set dynamic title

---
+++ web/src/components/head/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useAppSelector } from '../../redux/hooks';
+import { selectTimer } from '../../redux/timer';
+
+const Head: React.FC = () => {
+  const { solar, lunar, hour } = useAppSelector(selectTimer);
+
+  return (
+    <Helmet>
+      <title>{`☀️ ${solar} | 🕰 ${hour} | 🌙 ${lunar}`}</title>
+    </Helmet>
+  );
+};
+
+export default Head;
